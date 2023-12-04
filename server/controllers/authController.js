8908b7199dbd447f3f926f08b96615e4ac40c1c3
janidu98 <janinduchathushan@gmail.com>
2023-12-04 18:10:31 +0530

    add a middleware and a function to handle errors

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,7 +1,7 @@
 import User from "../models/userModel.js";
 import bcryptjs from 'bcryptjs';
 
-const signup = async (req, res) => {
+const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
     
     try {
@@ -13,7 +13,7 @@ const signup = async (req, res) => {
         res.status(201).json({ message: "User created successfully"});
 
     } catch (error) {
-        res.status(500).json(error.message);
+        next(error);
     }
 }
 
--- server/index.js
+++ server/index.js
@@ -18,6 +18,17 @@ app.use(express.json());
 app.use('/api/user', userRoutes)
 app.use('/api/auth', authRoutes)
 
+//middleware
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        message,
+        statusCode
+    })
+})
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
