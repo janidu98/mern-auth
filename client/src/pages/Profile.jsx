6e52f47b0fded9e165737fb86006420b8851be26
janidu98 <janinduchathushan@gmail.com>
2023-12-08 06:36:23 +0530

    complete image upload functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,14 +1,71 @@
 import { useSelector } from "react-redux"
+import { useEffect, useRef, useState } from "react";
+import {getDownloadURL, getStorage, ref, uploadBytesResumable} from "firebase/storage";
+import {app} from '../firebase';
 
 const Profile = () => {
 
   const {currentUser} = useSelector((state) => state.user);
+  const fileRef = useRef(null);
+  const [image, setImage] = useState(undefined);  //image
+  const [imagePercentage, setImagePercentage] = useState(0);
+  const [imageError, setImageError] = useState(false);
+  const [formData, setFormData] = useState({}); 
+
+  useEffect(() => {
+    if(image) {
+      handleFileUpload(image);
+    }
+  }, [image]);
+
+  const handleFileUpload = async (image) => {
+    const storage = getStorage(app); 
+    const fileName = new Date().getTime() + image.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, image);
+
+    uploadTask.on('state_changed',(snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setImagePercentage(Math.round(progress));
+      },
+      (error) => {
+        setImageError(true);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setFormData({ ...formData, profilePicture: downloadURL });
+        })
+      }
+    ); 
+  }
 
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className='text-3xl text-center font-semibold my-7'>Profile</h1>
       <form className="flex flex-col gap-4">
-        <img src={currentUser.profilePicture} alt="profile" className="h-24 w-24 self-center rounded-full object-cover mt-2"/>
+
+        <input type="file" ref={fileRef} hidden accept="/image/*" onChange={(e) => setImage(e.target.files[0])}/>
+        {/* 
+        firebase storage rules:
+          allow read;
+          allow write: if
+          request.resource.size < 2 * 1024 * 1024 &&
+          request.resource.contentType.matches('image/.*')
+        */}
+        <img src={currentUser.profilePicture} alt="profile" className="h-24 w-24 self-center rounded-full object-cover mt-2 cursor-pointer" onClick={() => fileRef.current.click()} />
+        <p className="text-sm self-center">
+          {imageError ? (
+            <span className="text-red-700">Error uploading image (file size must be less than 2MB)</span>
+          ) : (
+            imagePercentage > 0 && imagePercentage < 100 ? (
+              <span className="text-slate-700">{`Uploading: ${imagePercentage}%`}</span>
+            ) : (
+              imagePercentage === 100 ? (
+                <span className="text-green-700">Image uploaded successfully</span>
+              ) : ''
+            )
+          )}
+        </p>
 
         <input defaultValue={currentUser.username} type='text' id="username" placeholder="Username" className="bg-slate-100 rounded-lg p-3"/>
 
