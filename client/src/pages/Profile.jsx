d183cece2c9cce2681ea8e46f62ea5ce9e51a01c
janidu98 <janinduchathushan@gmail.com>
2023-12-09 06:44:15 +0530

    add signout user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -3,7 +3,7 @@ import { useEffect, useRef, useState } from "react";
 import {getDownloadURL, getStorage, ref, uploadBytesResumable} from "firebase/storage";
 import {app} from '../firebase';
 import { useDispatch } from "react-redux";
-import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserStart, deleteUserFailure, deleteUserSuccess } from "../redux/user/userSlice";
+import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserStart, deleteUserFailure, deleteUserSuccess, signOut } from "../redux/user/userSlice";
 
 const Profile = () => {
 
@@ -98,6 +98,16 @@ const Profile = () => {
     }
   }
 
+  const handleSignOut = async () => {
+    try {
+      await fetch('/api/auth/signout');
+      dispatch(signOut());
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className='text-3xl text-center font-semibold my-7'>Profile</h1>
@@ -138,7 +148,7 @@ const Profile = () => {
 
       <div className="flex justify-between mt-5">
         <span onClick={handleDeleteAccount} className="text-red-700 cursor-pointer">Delete Account</span>
-        <span className="text-red-700 cursor-pointer">Sign out</span>
+        <span onClick={handleSignOut} className="text-red-700 cursor-pointer">Sign out</span>
       </div>
 
       <p className="text-red-700 mt-5">{error && 'Something went wrong!'}</p>
--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -80,3 +80,6 @@ export const google = async (req, res, next) => {
     }
 }
 
+export const signout = async (req, res) => {
+    res.clearCookie('access_token').status(200).json('Signout success');
+}
